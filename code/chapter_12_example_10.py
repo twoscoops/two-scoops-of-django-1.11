892e7a628f027c3c2bc61104aa086c7e1f63aa20
Kamil Kijak <kamilkijak@gmail.com>
2017-12-09 21:02:24 +0100

    Fix indentation and line length

--- code/chapter_12_example_10.py
+++ code/chapter_12_example_10.py
@@ -39,10 +39,11 @@ given here, please contact us at info@twoscoopspress.org.
 from django.forms.widgets import TextInput
 
 class IceCreamFlavorInput(TextInput):
-  """Ice cream flavors must always end with 'Ice Cream'"""
+    """Ice cream flavors must always end with 'Ice Cream'"""
 
     def get_context(self, name, value, attrs):
-        context = super(IceCreamInput, self).get_context(name, value, attrs)
+        context = \
+            super(IceCreamInput, self).get_context(name, value, attrs)
         value = context['widget']['value']
         if not value.strip().lower().endswith('ice cream'):
             context['widget']['value'] = '{} Ice Cream'.format(value)
